5a7f1196b14d35cd9db8a876682fbe984167af8b
nalinluthra <nalin.luthra@gmail.com>
2020-11-28 11:56:23 +0530

    Multi File Support

--- server.js
+++ server.js
@@ -5,14 +5,6 @@ const app = express()
     // import { parse } from 'subtitle'
 var ss = require('subtitle')
 
-// console.log(ss.parse)
-
-
-
-const file = fs.readFileSync('assets/Rewari2.SRT', 'utf8');
-const data = ss.parse(file)
-console.log(data);
-
 
 app.use(express.static(path.join(__dirname, 'public')))
 
@@ -20,12 +12,52 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.htm'))
 })
 
+app.get('/player', function(req, res) {
+    console.log(req.query)
+    const path = 'assets/' + req.query.name;
+    const stat = fs.statSync(path)
+    const fileSize = stat.size
+    const range = req.headers.range
+
+    if (range) {
+        const parts = range.replace(/bytes=/, "").split("-")
+        const start = parseInt(parts[0], 10)
+        const end = parts[1] ?
+            parseInt(parts[1], 10) :
+            fileSize - 1
+
+        if (start >= fileSize) {
+            res.status(416).send('Requested range not satisfiable\n' + start + ' >= ' + fileSize);
+            return
+        }
+
+        const chunksize = (end - start) + 1
+        const file = fs.createReadStream(path, { start, end })
+        const head = {
+            'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+            'Accept-Ranges': 'bytes',
+            'Content-Length': chunksize,
+            'Content-Type': 'video/mp4',
+        }
+
+        res.writeHead(206, head)
+        file.pipe(res)
+    } else {
+        const head = {
+            'Content-Length': fileSize,
+            'Content-Type': 'video/mp4',
+        }
+        res.writeHead(200, head)
+        fs.createReadStream(path).pipe(res)
+    }
+})
+
 app.get('/data/', function(req, res) {
     res.send(data)
 })
 
 app.get('/video', function(req, res) {
-    const path = 'assets/sample.mp4'
+    const path = 'assets/test.mp4'
     const stat = fs.statSync(path)
     const fileSize = stat.size
     const range = req.headers.range
